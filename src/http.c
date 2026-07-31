09108e5ef73555becb025610b19d384370208c23
Kyrope <ky.the.rope@gmail.com>
2026-08-01 00:49:56 +0530

    AoS Chat code updated

---
+++ src/http.c
@@ -0,0 +1,31 @@
+
+/*
+	Copyright (c) 2017-2020 ByteBit
+
+	This file is part of AoSChat.
+
+	AoSChat is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	AoSChat is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with AoSChat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define HTTP_IMPLEMENTATION
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0501
+#endif
+
+#include <stdint.h>
+
+#include "http.h"
+
+
